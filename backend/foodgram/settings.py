b6c2d75522804b2edccc8c2e7ac671a8ec6986cb
Илья Козырев <job2006-78@yandex.ru>
2023-05-21 18:17:04 +0300

    Move secret key

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -13,8 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o@tb!x0z3z%cy9!@x1v9-=9ni'
-'(^u-2k9r*fzq386d5!4cytafk'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
